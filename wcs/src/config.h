11655f9b3d6b3003c7db823eb2dc7655f27ab569
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-08-11 15:54:24 +0200

    removed unused macros. Modified used global variable

--- wcs/src/config.h
+++ wcs/src/config.h
@@ -1,17 +1,21 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
+#define BUTTON_PIN 2
 #define MOTOR_PIN 9
 #define POT_PIN A0
 
-typedef enum
-{
-    UNCONNECTED,
-    AUTOMATIC,
-    MANUAL
-} wcs_state;
-
-extern wcs_state state;
-extern uint8_t sm_degree;
+// Stati del Subsystem WCS
+    typedef enum {
+        AUTOMATIC,
+        MANUAL,
+        UNCONNECTED
+    } wcs_state_t;
+    
+    // Variabili globali condivise tra i Task. Definita nel main e accessibile a tutti
+    extern wcs_state_t currentState;
+    //utilizzo di uint8_t per rappresentare la percentuale di apertura della valvola (0-100): permette di rappresentare sufficiente range di valori e risparmia memoria
+    extern uint8_t currentValveOpening; // Percentuale apertura valvola (0 - 100%)
+    extern uint8_t targetValveOpening;  // Apertura desiderata in AUTOMATIC (impostata da CUS)
 
 #endif
